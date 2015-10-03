5e465f851fa3183291374fcf6dbdc20bd064c935
aspen <pandilwar605@gmail.com>
2015-10-03 18:49:34 +0530

    first commit

---
+++ static/js/words-jquery.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	function playWord(url,mean){
+		$("#audio_word").attr('src', url).trigger('play');
+		 $("#mone").html(mean)
+		//var a = new Audio(url);
+		//a.play();
+	}
+	//alert("something");
+	$.get(SANSWER_POST, {}, function(data) {
+		playWord(data['next'],data['mean']);
+
+	});
+	$('#my_form').submit(function(event){
+		event.preventDefault();
+		var input_word = $('#input_word');
+		var user_input_word = input_word.val();
+		input_word.val('');
+		$.post(SANSWER_POST,{inputWord:user_input_word}, function (data){
+			if(data['done']){
+				//redirect here!
+				//alert("you are done!");
+				var redirectionUrl = data['next'];
+				window.location.replace(redirectionUrl);
+			}
+			else{
+				//alert("hello");				
+				playWord(data['next'],data['mean']);
+			}
+		});
+
+	});
+});
